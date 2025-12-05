c24aaf1c0f23a0030215f5b0da011027a2209ff3
zero <8.omkaryadav@gmail.com>
2025-12-06 05:05:59 +0530

    Clean

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -3,7 +3,7 @@
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 20;       /* snap pixel */
-static const unsigned int gappx     = 0;        /* gaps between windows */
+static const unsigned int gappx     = 5;        /* gaps between windows */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
@@ -85,6 +85,8 @@ static const Rule rules[] = {
     /* Calculator */
     RULE(.class = "Galculator", .isfloating = 1)
 
+    /* Discord */
+    RULE(.class = "discord", .tags = 1 << 5, .switchtotag = 1)
 };
 
 /* layout(s) */
